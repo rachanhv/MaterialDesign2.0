1c8c8d7d8762bb8ed1ac14c9c032f5dc2b5e83ea
Rachan HV <rachanr7@gmail.com>
2019-03-05 00:07:37 +0530

    Material Design files added to github.

---
+++ app/src/main/java/com/example/meterialdesign/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.example.meterialdesign;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends Activity {
+
+    @BindView(R.id.pager)
+    ViewPager pager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+    private class MyPagerAdapter extends FragmentStatePagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            switch(pos) {
+
+                case 0:
+                    return new LoginFragment();
+                case 1:
+                    return new RegistrationFragment();
+
+                default: return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/meterialdesign/RegistrationFragment.java
@@ -0,0 +1,24 @@
+package com.example.meterialdesign;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegistrationFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration, container, false);
+    }
+
+}
